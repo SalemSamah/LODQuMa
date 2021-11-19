b8287834e116efa89685d3d05ad3eff40d9ccb66
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-19 13:39:03 +0000

    :heavy_plus_sign: `generateQuery` to the `datasets` controller

--- server/src/controllers/DataSets.ts
+++ server/src/controllers/DataSets.ts
@@ -1,5 +1,9 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IQueryResultBindings } from "@comunica/actor-init-sparql";
 import { Request, Response } from "express";
 import StatusCodes from "http-status-codes";
+import { execQuey, generateSparql } from "src/services/sparqlQuery";
 const { OK } = StatusCodes;
 
 /**
@@ -9,6 +13,26 @@ const { OK } = StatusCodes;
  * @param res
  * @returns
  */
-export const getDataSetsAsCsv = (req: Request, res: Response) => {
+const getDataSetsAsCsv = async (req: Request, res: Response) => {
+  const result = (await execQuey()) as IQueryResultBindings;
+  result.bindingsStream.on("data", (binding) => {
+    //console.log(binding.get("?s").value);
+    //console.log(binding.get("?s").termType);
+    //console.log(binding.get("?p").value);
+    //console.log(binding.get("?o").value);
+    /*console.log(
+      `${binding.get("?subject").value as string} --- ${
+        binding.get("?predicate").value as string
+      } --- ${binding.get("?object").value as string}`
+    );*/
+  });
+
   return res.status(OK).json({ data: "result" });
 };
+
+const generateQuery = (req: Request, res: Response) => {
+  const query = generateSparql();
+  return res.status(OK).json({ data: query });
+};
+
+export { getDataSetsAsCsv, generateQuery };
