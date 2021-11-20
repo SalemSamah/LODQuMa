4c7005539afd439b8d16a8105d08219db2d903b0
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-20 18:43:37 +0000

    :racehorse: implement `replaceURIbyPrefixe`

--- server/src/shared/functions.ts
+++ server/src/shared/functions.ts
@@ -1,3 +1,5 @@
+import { LOCALNAMESPACES } from 'src/config/SparqlQuery';
+import { RDFTerm } from 'src/interfaces/RDF';
 import logger from './Logger';
 
 export const pErr = (err: Error) => {
@@ -9,3 +11,14 @@ export const pErr = (err: Error) => {
 export const getRandomInt = () => {
     return Math.floor(Math.random() * 1_000_000_000_000);
 };
+
+export const replaceURIbyPrefixe = (rdfTerm: RDFTerm): RDFTerm => {
+    if (rdfTerm.termType === "NamedNode") {
+      for (const [key, value] of Object.entries(LOCALNAMESPACES)) {
+        if (rdfTerm.value.includes(value)) {
+          rdfTerm.value = rdfTerm.value.replace(value, `${key}:`);
+        }
+      }
+    }
+    return rdfTerm;
+  };
