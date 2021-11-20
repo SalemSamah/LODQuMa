3106dc04aba6ba3b25c0af8ff514b614d33b662b
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-20 18:39:33 +0000

    :zap: declare `LOCALNAMESPACES` as general constant

---
+++ server/src/config/SparqlQuery.ts
@@ -0,0 +1,15 @@
+/*
+ * @description Predefined Namespace Prefixes
+ * @readonly
+ * @name LOCALNAMESPACES
+ * source : https://www.orpha.net/sparql?nsdecl
+ * source : http://prefix.cc/
+ * @author  @fetahokey
+ * @author  @SalemSamah
+ */
+export const LOCALNAMESPACES = {
+  dbr: "http://dbpedia.org/resource/",
+  dbo: "http://dbpedia.org/ontology/",
+  dbp: "http://dbpedia.org/property/",
+  foaf: "http://xmlns.com/foaf/0.1/",
+};
