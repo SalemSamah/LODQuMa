ff135b0aaad145f77cd2887a3d3516154f48b78f
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-20 18:42:22 +0000

    :bulb: refactor `sparqlQuery` and Customize `QueryBuilder`

--- server/src/services/sparqlQuery.ts
+++ server/src/services/sparqlQuery.ts
@@ -1,147 +1,307 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable max-len */
 import { newEngine } from "@comunica/actor-init-sparql";
-import SparqlGenerator from "sparqljs";
-import SparqlParser from "sparqljs";
-import fs from "fs";
+import {
+  containerQueryLimit,
+  dbo,
+  dbp,
+  foaf,
+  innerQueryLimit,
+  object,
+  organisationDomain,
+  personDomain,
+  predicate,
+  subject,
+  type,
+  variable,
+  wikiPageExternalLink,
+  wikiPageID,
+  wikiPageRevisionID,
+  wikiPageWikiLink,
+  workDomain,
+} from "@shared/constants";
+import {
+  default as SparqlGenerator,
+  IriTerm,
+  SparqlQuery,
+  Variable,
+} from "sparqljs";
+import { LOCALNAMESPACES } from "src/config/SparqlQuery";
 
-const writable = fs.createWriteStream("res.json");
 const myEngine = newEngine();
 const generator = SparqlGenerator.Generator;
-const parser = SparqlParser.Parser;
 
-const execQuey = async () => {
+const execQuey = async (
+  domain: string,
+  containerQueryOffset: number | undefined
+) => {
   const result = await myEngine.query(
-    `
-    PREFIX dbo: <http://dbpedia.org/ontology/> 
-    PREFIX dbp: <http://dbpedia.org/property/> 
-    PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
-    SELECT
-    *
-    WHERE
-    {
-    ?subject ?predicate ?object.
-    FILTER( STRSTARTS(STR(?predicate),str(dbo:)) || STRSTARTS(STR(?predicate),str(dbp:)) || STRSTARTS(STR(?predicate),str(foaf:)) )
-    FILTER (!contains(STR(?predicate),"abstract") && !contains(STR(?predicate),"wikiPageExternalLink") && !contains(STR(?predicate),"wikiPageID") && !contains(STR(?predicate),"wikiPageRevisionID") && !contains(STR(?predicate),"wikiPageWikiLink"))
-    {
-    SELECT DISTINCT ?subject WHERE { ?subject a ?class. FILTER (?class in(dbo:Organisation)). } LIMIT 60000
-    }
-    }
-    OFFSET 0
-    LIMIT 10
-    
-    `,
+    generateSparql(domain, containerQueryOffset),
     {
       sources: ["https://dbpedia.org/sparql"],
     }
   );
-
-  const { data } = await myEngine.resultToString(
-    result,
-    "application/sparql-results+json"
-  );
-  data.pipe(writable);
-
   return result;
 };
 
-const generateSparql = () => {
-  // TODO: make use of this function to enhance query personalization
-  const query = {
+const generateSparql = (
+  domain: string,
+  containerQueryOffset: number | undefined
+) => {
+  const query: SparqlQuery = queryBuilder(domain, containerQueryOffset);
+  const generatedQuery = new generator().stringify(query);
+
+  return generatedQuery;
+};
+
+const queryBuilder = (
+  domain: string,
+  containerQueryOffset: number | undefined
+): SparqlQuery => {
+  let domainFilter: IriTerm[] = [];
+  switch (domain) {
+    case "work":
+      domainFilter = workDomain;
+      break;
+    case "person":
+      domainFilter = personDomain;
+      break;
+    case "organisation":
+      domainFilter = organisationDomain;
+  }
+
+  const query: SparqlQuery = {
     queryType: "SELECT",
-    variables: [
-      {
-        termType: "Variable",
-        value: "p",
-      },
-      {
-        termType: "Variable",
-        value: "c",
-      },
-    ],
+    variables: [subject as Variable, predicate as Variable, object as Variable],
     where: [
       {
         type: "bgp",
         triples: [
           {
-            subject: {
-              termType: "Variable",
-              value: "p",
-            },
-            predicate: {
-              termType: "NamedNode",
-              value: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
-            },
-            object: {
-              termType: "NamedNode",
-              value: "http://dbpedia.org/ontology/Artist",
-            },
+            subject,
+            predicate,
+            object,
           },
+        ],
+      },
+      {
+        type: "filter",
+        expression: {
+          type: "operation",
+          operator: "||",
+          args: [
+            {
+              type: "operation",
+              operator: "||",
+              args: [
+                {
+                  type: "operation",
+                  operator: "strstarts",
+                  args: [
+                    {
+                      type: "operation",
+                      operator: "str",
+                      args: [predicate],
+                    },
+                    {
+                      type: "operation",
+                      operator: "str",
+                      args: [dbo],
+                    },
+                  ],
+                },
+                {
+                  type: "operation",
+                  operator: "strstarts",
+                  args: [
+                    {
+                      type: "operation",
+                      operator: "str",
+                      args: [predicate],
+                    },
+                    {
+                      type: "operation",
+                      operator: "str",
+                      args: [dbp],
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: "operation",
+              operator: "strstarts",
+              args: [
+                {
+                  type: "operation",
+                  operator: "str",
+                  args: [predicate],
+                },
+                {
+                  type: "operation",
+                  operator: "str",
+                  args: [foaf],
+                },
+              ],
+            },
+          ],
+        },
+      },
+      {
+        type: "filter",
+        expression: {
+          type: "operation",
+          operator: "&&",
+          args: [
+            {
+              type: "operation",
+              operator: "&&",
+              args: [
+                {
+                  type: "operation",
+                  operator: "&&",
+                  args: [
+                    {
+                      type: "operation",
+                      operator: "&&",
+                      args: [
+                        {
+                          type: "operation",
+                          operator: "!",
+                          args: [
+                            {
+                              type: "operation",
+                              operator: "contains",
+                              args: [
+                                {
+                                  type: "operation",
+                                  operator: "str",
+                                  args: [predicate],
+                                },
+                                wikiPageWikiLink,
+                              ],
+                            },
+                          ],
+                        },
+                        {
+                          type: "operation",
+                          operator: "!",
+                          args: [
+                            {
+                              type: "operation",
+                              operator: "contains",
+                              args: [
+                                {
+                                  type: "operation",
+                                  operator: "str",
+                                  args: [predicate],
+                                },
+                                wikiPageExternalLink,
+                              ],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                    {
+                      type: "operation",
+                      operator: "!",
+                      args: [
+                        {
+                          type: "operation",
+                          operator: "contains",
+                          args: [
+                            {
+                              type: "operation",
+                              operator: "str",
+                              args: [predicate],
+                            },
+                            wikiPageID,
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+                {
+                  type: "operation",
+                  operator: "!",
+                  args: [
+                    {
+                      type: "operation",
+                      operator: "contains",
+                      args: [
+                        {
+                          type: "operation",
+                          operator: "str",
+                          args: [predicate],
+                        },
+                        wikiPageRevisionID,
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: "operation",
+              operator: "!",
+              args: [
+                {
+                  type: "operation",
+                  operator: "contains",
+                  args: [
+                    {
+                      type: "operation",
+                      operator: "str",
+                      args: [predicate],
+                    },
+                    wikiPageWikiLink,
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      },
+      {
+        type: "group",
+        patterns: [
           {
-            subject: {
-              termType: "Variable",
-              value: "p",
-            },
-            predicate: {
-              termType: "NamedNode",
-              value: "http://dbpedia.org/ontology/birthPlace",
-            },
-            object: {
-              termType: "Variable",
-              value: "c",
-            },
-          },
-          {
-            subject: {
-              termType: "Variable",
-              value: "c",
-            },
-            predicate: {
-              termType: "NamedNode",
-              value: "http://xmlns.com/foaf/0.1/name",
-            },
-            object: {
-              termType: "Literal",
-              value: "York",
-              language: "en",
-              datatype: {
-                termType: "NamedNode",
-                value: "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString",
+            queryType: "SELECT",
+            distinct: true,
+            variables: [subject],
+            where: [
+              {
+                type: "bgp",
+                triples: [
+                  {
+                    subject,
+                    predicate: type,
+                    object: variable,
+                  },
+                ],
               },
-            },
+              {
+                type: "filter",
+                expression: {
+                  type: "operation",
+                  operator: "in",
+                  args: [variable, domainFilter],
+                },
+              },
+            ],
+            limit: innerQueryLimit,
+            type: "query",
+            prefixes: {},
           },
         ],
       },
     ],
+    limit: containerQueryLimit,
+    offset: containerQueryOffset,
     type: "query",
-    prefixes: {
-      "dbpedia-owl": "http://dbpedia.org/ontology/",
-    },
+    prefixes: LOCALNAMESPACES,
   };
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  const generatedQuery = new generator().stringify({ query } as any);
-  const sparqlQuery = `
-      PREFIX dbo: <http://dbpedia.org/ontology/> 
-      PREFIX dbp: <http://dbpedia.org/property/> 
-      PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
-      SELECT
-        *
-        WHERE
-        {
-        ?subject ?predicate ?object.
-        FILTER( STRSTARTS(STR(?predicate),str(dbo:)) || STRSTARTS(STR(?predicate),str(dbp:)) || STRSTARTS(STR(?predicate),str(foaf:)) )
-        FILTER (!contains(STR(?predicate),"abstract") && !contains(STR(?predicate),"wikiPageExternalLink") && !contains(STR(?predicate),"wikiPageID") && !contains(STR(?predicate),"wikiPageRevisionID") && !contains(STR(?predicate),"wikiPageWikiLink"))
-        {
-        #SELECT DISTINCT ?subject WHERE { ?subject a ?class. FILTER (?class in(wikidata:Q5, foaf:Person, schema:Person, dbo:Perosn)). } LIMIT 40827
-        #SELECT DISTINCT ?subject WHERE { ?subject a ?class. FILTER (?class in(dbo:Work)). } LIMIT 50000
-        SELECT DISTINCT ?subject WHERE { ?subject a ?class. FILTER (?class in(dbo:Organisation)). } LIMIT 60000
-        }
-        }
-        LIMIT 100
-  
-  `;
-  const parsedQuery = new parser().parse(sparqlQuery);
 
-  return parsedQuery;
+  return query;
 };
 export { execQuey, generateSparql };
