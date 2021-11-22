7026761ae56c57f953367456012de3c4aade7fe3
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-22 17:06:04 +0000

    :lipstick:  add sleep  function

--- server/src/shared/functions.ts
+++ server/src/shared/functions.ts
@@ -1,24 +1,28 @@
-import { LOCALNAMESPACES } from 'src/config/SparqlQuery';
-import { RDFTerm } from 'src/interfaces/RDF';
-import logger from './Logger';
+import { LOCALNAMESPACES } from "src/config/SparqlQuery";
+import { RDFTerm } from "src/interfaces/RDF";
+import logger from "./Logger";
 
 export const pErr = (err: Error) => {
-    if (err) {
-        logger.err(err);
-    }
+  if (err) {
+    logger.err(err);
+  }
 };
 
 export const getRandomInt = () => {
-    return Math.floor(Math.random() * 1_000_000_000_000);
+  return Math.floor(Math.random() * 1_000_000_000_000);
 };
 
 export const replaceURIbyPrefixe = (rdfTerm: RDFTerm): RDFTerm => {
-    if (rdfTerm.termType === "NamedNode") {
-      for (const [key, value] of Object.entries(LOCALNAMESPACES)) {
-        if (rdfTerm.value.includes(value)) {
-          rdfTerm.value = rdfTerm.value.replace(value, `${key}:`);
-        }
+  if (rdfTerm.termType === "NamedNode") {
+    for (const [key, value] of Object.entries(LOCALNAMESPACES)) {
+      if (rdfTerm.value.includes(value)) {
+        rdfTerm.value = rdfTerm.value.replace(value, `${key}:`);
       }
     }
-    return rdfTerm;
-  };
+  }
+  return rdfTerm;
+};
+
+export const sleep = async (millis: number) => {
+  return new Promise((resolve) => setTimeout(resolve, millis));
+};
