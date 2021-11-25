d609f5defd8b8f9496d93fff84b71e07a9782b04
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-25 16:39:47 +0000

    🔨 update the index router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import userRouter from "./users";
 import dataRouter from "./datasets";
+import preprocessRouter from "./preprocess";
 
 // Export the base-router
 const baseRouter = Router();
 // User-route
 baseRouter.use("/users", userRouter);
-// Other-route
+// data-route
 baseRouter.use("/data", dataRouter);
+// preprocess-route
+// Data set pre processing
+baseRouter.use("/dpp", preprocessRouter);
 
 export default baseRouter;
