ededa2c44829022ed03b7aeea1606f074d13bc5c
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-27 19:52:09 +0000

    :heavy_check_mark: update synonyms routes

--- server/src/routes/synonyms.ts
+++ server/src/routes/synonyms.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { generateSynonymsDictionary, generateQVCs } from "../controllers/Synonyms";
+import {
+  generateSynonymsDictionary,
+  generateQVCs,
+  saveDistinctPredicates,
+  cleanDictionary,
+} from "../controllers/Synonyms";
 
 // Synonyms-route
 const synonymsRouter = Router();
 
 synonymsRouter.post("/dictionary", generateSynonymsDictionary);
+synonymsRouter.post("/saveDistinct", saveDistinctPredicates);
+synonymsRouter.post("/clean", cleanDictionary);
 synonymsRouter.post("/qvc", generateQVCs);
-
 
 export default synonymsRouter;
