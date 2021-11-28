f1c3ee6ea92c4fd7c7500c5decce9802ef87b1bc
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-28 18:10:51 +0000

    :sparkles: save Rare Predicates `route`

--- server/src/routes/synonyms.ts
+++ server/src/routes/synonyms.ts
@@ -4,6 +4,7 @@ import {
   generateQVCs,
   saveDistinctPredicates,
   cleanDictionary,
+  saveRarePredicates,
 } from "../controllers/Synonyms";
 
 // Synonyms-route
@@ -12,6 +13,7 @@ const synonymsRouter = Router();
 synonymsRouter.post("/dictionary", generateSynonymsDictionary);
 synonymsRouter.post("/saveDistinct", saveDistinctPredicates);
 synonymsRouter.post("/clean", cleanDictionary);
+synonymsRouter.post("/rare", saveRarePredicates);
 synonymsRouter.post("/qvc", generateQVCs);
 
 export default synonymsRouter;
