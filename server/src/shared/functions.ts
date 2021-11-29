2bb92f1d340daca9dc510aed176c78eeb5736209
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-29 21:31:30 +0000

    :rocket: add shared function `diffrence` btwen obj {RDFTriples}

--- server/src/shared/functions.ts
+++ server/src/shared/functions.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import _ from "lodash";
 import { LOCALNAMESPACES } from "src/config/SparqlQuery";
 import { RDFTerm } from "src/interfaces/RDF";
 import logger from "./Logger";
@@ -25,4 +27,24 @@ export const replaceURIbyPrefixe = (rdfTerm: RDFTerm): RDFTerm => {
 
 export const sleep = async (millis: number) => {
   return new Promise((resolve) => setTimeout(resolve, millis));
+};
+
+/**
+ * Deep diff between two object, using lodash
+ * @param  {Object} object Object compared
+ * @param  {Object} base   Object to compare with
+ * @return {Object}        Return a new object who represent the diff
+ */
+export const difference = (object: any, base: any) => {
+  const changes = (object: any, base: any) => {
+    return _.transform(object,  (result: any, value, key) =>{
+      if (!_.isEqual(value, base[key])) {
+        result[key] =
+          _.isObject(value) && _.isObject(base[key])
+            ? changes(value, base[key])
+            : value;
+      }
+    });
+  };
+  return changes(object, base);
 };
